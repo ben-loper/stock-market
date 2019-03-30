738b6cd4b19ce9ccd828a359a9a5ddf46b104a4e
johnwunderle <wunderlejohn@yahoo.com>
2019-03-30 14:56:39 -0400

    portfolio view adj

---
+++ StockMarket/StockMarket/Models/PortfolioViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockMarket.Models
+{
+    public class PortfolioViewModel
+    {
+        public string Symbol { get; set; }
+        public DateTime Date { get; set; }
+        public int NumberOfShares { get; set; }
+        public decimal Price { get; set; }
+    }
+}
