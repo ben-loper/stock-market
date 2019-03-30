e769156f3a23f097d12f94f6f9edb12f56c4e260
johnwunderle <wunderlejohn@yahoo.com>
2019-03-30 17:56:56 -0400

    fetching stuff

--- StockMarket/StockMarket/wwwroot/js/Data.js
+++ StockMarket/StockMarket/wwwroot/js/Data.js
@@ -1,26 +1,42 @@
 ﻿
+//const request = async () => {
+//    const response = await fetch('https://api.com/values/1');
+//    const json = await response.json();
+//    console.log(json);
+//}
+
+//request();
+
+
+
+
 function getStockInfo() {
-    return fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo')
+    return fetch('/js/dummy-data.json')
         .then((res) => {
             return res.json();
         })
         .then(function (myJson) {
-            console.log(myJson["Time Series (Daily)"]["2018-11-02"]);
+            return myJson["Time Series (Daily)"]["2018-11-02"];
         });
 }
 
 function updateTable(jsonObject) {
     // Get the table by id
-
+    const tableNode = document.getElementById('dataTable');
 
     // Create a new table row
+    const tableRowNode = document.createElement('tr');
 
     // For each JSON property, Create a td 
+    const tableDataNode = document.createElement('td');
+    jsonObject.forEach((element) => {
+        tableDataNode.innerText = element;
 
         // Attach to the td node the td for each value
-
+        tableRowNode.insertAdjacentElement('beforeend', tableDataNode);
+    });       
     // Attach the table row to the table
-
+    tableNode.insertAdjacentElement('beforeend', tableRowNode);
 }
 
 
@@ -31,7 +47,10 @@ document.addEventListener('DOMContentLoaded', () => {
     submitButton.addEventListener('click', (event) => {
         event.preventDefault();
 
+
         let jsonObject = getStockInfo();
+
+
 
         updateTable(jsonObject);
     });
